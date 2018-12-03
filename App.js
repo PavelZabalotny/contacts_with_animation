9a7ac2d864a45bbcff7b57181e3f1cd9cffae584
Pavel Zabolotny <pavelzabalotny@gmail.com>
2018-12-03 19:14:19 +0300

    получение списка контактов

--- App.js
+++ App.js
@@ -6,44 +6,113 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react"
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  PermissionsAndroid
+} from "react-native"
+import Contacts from "react-native-contacts"
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  state = {
+    isButtonPress: false
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  async requestContactsPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+        {
+          title: "Contacts app",
+          message: "Contacts app needs access to your contacts"
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the contacts")
+      } else {
+        console.log("Contacts permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
+  async requestProfilePermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+        {
+          title: "Contacts app",
+          message: "Contacts app needs access to your profile"
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the profile")
+      } else {
+        console.log("Profile permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
+  componentDidMount = () => {
+    this.requestContactsPermission()
+    this.requestProfilePermission()
+  }
+
   render() {
+    const { isButtonPress } = this.state
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+          onPress={this.onPressGetContacts}
+          title="Get contacts"
+          color="#841584"
+          accessibilityLabel="Get contacts from your phone"
+        />
+        {isButtonPress ? (
+          <Text>Contacts</Text>
+        ) : (
+          <Text>Press button to get contacts</Text>
+        )}
       </View>
-    );
+    )
+  }
+
+  onPressGetContacts = () => {
+    this.setState({
+      isButtonPress: !this.state.isButtonPress
+    })
+
+    Contacts.getAll((err, contacts) => {
+      if (err) throw err
+
+      // contacts returned
+      console.log(contacts)
+    })
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // justifyContent: "center",
+    // alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+})
